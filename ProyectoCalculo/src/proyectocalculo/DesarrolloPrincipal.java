7c369a74b23bb050334fceb85040a8650cd49951
juanyanza11 <ww4fjj@gmail.com>
2019-05-12 13:08:16 -0500

    Interfaz y funcion 5

--- ProyectoCalculo/src/proyectocalculo/DesarrolloPrincipal.java
+++ ProyectoCalculo/src/proyectocalculo/DesarrolloPrincipal.java
@@ -1,17 +1,9 @@
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package proyectocalculo;
-
-import funciones.*;
 import java.util.Scanner;
-import proyectocalculo.*;
+
 /**
  *
- * @author DAVID
+ * @author grupal
  */
 public class DesarrolloPrincipal {
     /**
@@ -34,18 +26,23 @@ public class DesarrolloPrincipal {
         String respuesta = "Respuesta: \n   X | Y   \n";
         respuesta = String.format("%s¯¯¯¯¯¯¯¯¯¯¯¯\n", respuesta);
         do{
-        System.out.println("---- FUNCIONES ----");
-        System.out.println("Ejila una de las siguientes f1unciones");
-        System.out.println(" 1.) f(x) = 2x^3 - 6x^2 +20x-1");
-        System.out.println(" 2.) f(x) = 6x^2 - 3 / 25");
-        System.out.println(" 3.) f(x) = x^4 -8 / 3x^2+5");
-        System.out.println(" 4.) f(x) = x^3 +25x+8");
-        System.out.println(" 5.) f(x) = 3x^2 +5x+3/2x");
-        System.out.println(" 6.) f(x) = 3x^3 +5x^2 +60 ");
-        System.out.println(" 7.) f(x) = x^3 +3x^2 +2x+1");
-        System.out.println(" 8.) f(x) = 2x*(x^3 +23) ");
-        System.out.println(" 9.) f(x) = (10x+3x^3 ) -2");
-        System.out.println(" 10.) f(x) = 3^3x+2 +5x-35 \n");
+        System.out.println("     _______________________");
+        System.out.println("    "+"|       FUNCIONES       |");
+        System.out.println("     ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯");
+        System.out.println("Ejila una de las siguientes f1unciones\n");
+        
+        System.out.println("|¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯|");
+        System.out.println("| 1.) f(x) = 2x^3 - 6x^2 +20x-1     |");
+        System.out.println("| 2.) f(x) = 6x^2 - 3 / 25          |");
+        System.out.println("| 3.) f(x) = x^4 -8 / 3x^2+5        |");
+        System.out.println("| 4.) f(x) = x^3 +25x+8             |");
+        System.out.println("| 5.) f(x) = 3x^2 + 5x+3/2x         |");
+        System.out.println("| 6.) f(x) = 3x^3 +5x^2 +60         |");
+        System.out.println("| 7.) f(x) = x^3 +3x^2 +2x+1        |");
+        System.out.println("| 8.) f(x) = 2x*(x^3 +23)           |");
+        System.out.println("| 9.) f(x) = (10x+3x^3 ) -2         |");
+        System.out.println("| 10.) f(x) = 3^3x+2 +5x-35         |");
+        System.out.println("|___________________________________|");
         menu = entrada.nextInt();
         
         while(menu < 1 || menu >10){
@@ -91,7 +88,10 @@ public class DesarrolloPrincipal {
                 System.out.printf("%s", respuesta);
                 break;
             case 5:
-                
+                for (x = inicio; x <= fin; x++){
+                    y= 3*Math.pow(x,2)+(5*x+3)/(2*x);
+                    respuesta = String.format("%s%.2f | %.2f    \n",respuesta, x, y);
+                }
                 break;
             case 6:
                 for (x = inicio;  x<= fin; x++) {
